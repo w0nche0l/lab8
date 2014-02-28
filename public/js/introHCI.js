d3656e4e224a81d7d7d588a496537f239e50a411
vagrant <vagrant@precise32.(none)>
2014-02-28 21:16:42 +0000

    finish part1

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,6 +3,10 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$('.likeBtn').click(function(e){
+		e.preventDefault();
+		ga("send", "event", "like", "click");
+	})
 });
 
 /*
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	projects.grid = false;
   	res.render('index', projects);
 };
+exports.viewGrid = function(req,res){
+	projects.grid= true;
+	console.log(projects);
+	res.render('index', projects);
+}
